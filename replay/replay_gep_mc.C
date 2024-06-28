5ef718d2e6f7554b5ace75e176ed9266e375d8d2
Eric Fuchey <efuchey@jlab.org>
2024-06-27 20:57:18 -0400

    AAdded more crates in db crate map (useful???) Added a (currently empty) db for earm (gep electron arm)

--- replay/replay_gep_mc.C
+++ replay/replay_gep_mc.C
@@ -39,15 +39,14 @@ TDatime get_datime(uint gepconfig)
 
 void replay_gep_mc(const char* filebase, uint gepconfig, uint nev = -1, TString experiment="gep")
 {
-  //SBSEArm* earm = new SBSEArm("earm", "GEP electron arm" );
+  SBSEArm* earm = new SBSEArm("earm", "GEP electron arm" );
   //earm->AddDetector( new SBSCalorimeter("ecal", "ECal") );
   //earm->AddDetector( new SBSCDet("cdet", "coordinate detector") );
-  //gHaApps->Add(earm);
-  
+  gHaApps->Add(earm);
   SBSEArm *harm = new SBSEArm("sbs","Hadron Arm with HCal");
   harm->AddDetector( new SBSHCal("hcal","HCAL") );
-  harm->AddDetector( new SBSGEMSpectrometerTracker("ft", "Front tracker") );
-  //harm->AddDetector( new SBSGEMSpectrometerTracker("fpp", "Focal Plane Polarimeter") );
+  harm->AddDetector( new SBSGEMSpectrometerTracker("gemFT", "Front tracker") );
+  //harm->AddDetector( new SBSGEMSpectrometerTracker("gemFPP", "Focal Plane Polarimeter") );
   gHaApps->Add(harm);
 
   //bigbite->SetDebug(2);
